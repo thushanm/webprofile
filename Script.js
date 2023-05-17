904a2ddf450ea7894b3bae5ab3e4085b8e8ce322
thushanm <122750947+thushanm@users.noreply.github.com>
2023-05-17 21:59:02 +0530

    name animation

--- Script.js
+++ Script.js
@@ -4,5 +4,22 @@ let typed=new Typed(".autoType",{
     typeSpeed:150,
     backSpeed:150,
     loop:true
+});/*Auto tYping*/
+    AOS.init();/*page animation*/
+var textWrapper = document.querySelector('#mainArticle2 #mainArticle2s2');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span #mainArticle2s2>$&</span>");
+
+anime.timeline({loop: true})
+    .add({
+        targets: '#mainArticle2 #mainArticle2s2',
+        translateY: ["1.1em", 0],
+        translateZ: 0,
+        duration: 750,
+        delay: (el, i) => 50 * i
+    }).add({
+    targets: '#mainArticle2',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
 });
-console.log("helo bro");
