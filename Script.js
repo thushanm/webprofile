8ef01d516e1adbb3d21e9db5b3cf39565b53e1cc
thushanm <122750947+thushanm@users.noreply.github.com>
2023-05-17 19:06:21 +0530

    auto type

---
+++ Script.js
@@ -0,0 +1,8 @@
+
+let typed=new Typed(".autoType",{
+    strings:["Web Devoloper","Stalog Software Devoloper","Mobile App Devoloper"],
+    typeSpeed:150,
+    backSpeed:150,
+    loop:true
+});
+console.log("helo bro");
