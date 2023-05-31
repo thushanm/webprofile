1f8f581b17bff2b41e762092193839168a3ebbb4
thushanm <122750947+thushanm@users.noreply.github.com>
2023-05-31 16:27:15 +0530

    profile chage

--- Script.js
+++ Script.js
@@ -1,6 +1,6 @@
 
 let typed=new Typed(".autoType",{
-    strings:["Web Devoloper","Stalog Software Devoloper","Mobile App Devoloper"],
+    strings:["Standalone software Developer","Web Application Developer","Mobile Application Developer"],
     typeSpeed:150,
     backSpeed:150,
     loop:true
@@ -36,11 +36,33 @@ anime.timeline({loop: true})
         })
     }
 
+//////////////////////////////////////////////////////////////////// clock///////////////////
+function showTime(){
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    var session = "AM";
 
+    if(h == 0){
+        h = 12;
+    }
 
+    if(h > 12){
+        h = h - 12;
+        session = "PM";
+    }
 
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
 
+    var time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("MyClockDisplay").innerText = time;
+    document.getElementById("MyClockDisplay").textContent = time;
 
+    setTimeout(showTime, 1000);
 
+}
 
-////////////////////////////////////////clock/////////////////////////////////////////////
+showTime();
