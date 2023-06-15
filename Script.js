5c7003d6642ca7f197667fa41eecdb7e27882ff7
thushanm <122750947+thushanm@users.noreply.github.com>
2023-06-15 23:02:46 +0530

    form

--- Script.js
+++ Script.js
@@ -36,4 +36,18 @@ anime.timeline({loop: true})
         })
     }
 
-//////////////////////////////////////////////////////////////////// 3589684  ///////////////////
+//////////////////////////////////////////////////////////////////// 3589684  //////////////////
+function sendEmail() {
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "username",
+        Password : "password",
+        To : 'them@website.com',
+        From : "you@isp.com",
+        Subject : "This is the subject",
+        Body : "And this is the body"
+    }).then(
+        message => alert(message)
+    );
+
+}
